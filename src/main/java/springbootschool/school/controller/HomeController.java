29ee87a5f9c201e6c21d617f2727ed8d5d39a145
Marcin Rojek <marcin.rojek1985@gmail.com>
2018-07-05 01:00:56 +0200

    Last changes

--- src/main/java/springbootschool/school/controller/HomeController.java
+++ src/main/java/springbootschool/school/controller/HomeController.java
@@ -1,4 +1,29 @@
 package springbootschool.school.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String helloWorld() {
+
+        return "FOR EVERYONE";
+    }
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(){
+
+        return "Admin panel";
+    }
+
+    @GetMapping("/remove")
+    @ResponseBody
+    public String remove(){
+
+        return "Remove";
+    }
 }
