46c05a809ab87b32c7cb5f7ce80fc7390185b86b
Marcin Rojek <marcin.rojek1985@gmail.com>
2018-07-11 23:31:27 +0200

    Thymeleaf and controller update

--- src/main/java/springbootschool/school/controller/ExamsController.java
+++ src/main/java/springbootschool/school/controller/ExamsController.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import springbootschool.school.entity.Exams;
+import springbootschool.school.entity.StudentList;
 import springbootschool.school.repository.ExamsRepository;
+import springbootschool.school.repository.StudentListRepository;
+
+import java.util.List;
 
 
 @Controller
@@ -16,6 +20,8 @@ public class ExamsController {
     @Autowired
     ExamsRepository examsRepository;
 
+    @Autowired
+    StudentListRepository studentListRepository;
 
     @GetMapping("/all")
     public String showAll(Model model) {
@@ -28,13 +34,21 @@ public class ExamsController {
     public String userForm(Model model) {
 
         model.addAttribute("exams", new Exams());
+/*
+        model.addAttribute("students",students());
+*/
         return "ExamsForm";
 }
 
     @PostMapping("/add")
-    public String egzamForm(@ModelAttribute Exams exams) {
+    public String examForm(@ModelAttribute Exams exams) {
+
             examsRepository.save(exams);
-        return "redirect:/user/all";
+        return "redirect:/exam/all";
     }
 
+    @ModelAttribute("students")
+    public List <StudentList> students(){
+        return  studentListRepository.findAll();
+    }
 }
